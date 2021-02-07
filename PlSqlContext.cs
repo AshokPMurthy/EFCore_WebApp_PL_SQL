e8e45376757eac0c2c3361bd95d6ed5ccf2efa86
AshokPMurthy <78699585+AshokPMurthy@users.noreply.github.com>
2021-02-07 10:45:32 -0800

    Add files via upload

---
+++ PlSqlContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using EFCore_WebApp_PL_SQL.Models;
+
+namespace EFCore_WebApp_PL_SQL.DAL
+{
+    public class PlSqlContext : DbContext
+    {
+        //public DbSet<EmpListByDeptName> EmpLists { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+        public PlSqlContext(
+            DbContextOptions<PlSqlContext> options) : base(options)
+        {
+        }
+    }
+
+}
